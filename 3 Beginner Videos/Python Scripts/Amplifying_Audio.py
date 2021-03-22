305dbe6c6e52d46b40700d1434f433c741473a7b
Calvin Skalla <calvin-skalla@uiowa.edu>
2021-03-22 07:49:58 -0500

    Found a way to amplify dB using ffmpeg
    Former-commit-id: 43f56deb3992b48301a852da90929406cbfcef58

---
+++ 3 Beginner Videos/Python Scripts/Amplifying_Audio.py
@@ -0,0 +1,25 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+#Changing format from MP3 to Wav
+vid_1_src = "C:/Users/calvi/OneDrive - University of Iowa/Documents/aerobictextreview/3 Beginner Videos/Videos/Video_1_trimmed.mp3"
+
+vid_1_dst = "C:/Users/calvi/OneDrive - University of Iowa/Documents/aerobictextreview/3 Beginner Videos/Videos/Video_1_trimmed.wav"
+
+sound_vid_1 = AudioSegment.from_mp3(vid_1_src)
+sound_vid_1.export(vid_1_dst, format="wav")
+sound_vid_1 = AudioSegment.from_wav(vid_1_dst)
+
+#Length in Seconds
+vid_1_duration = (len(sound_vid_1)/1000)
+
+vid_1 = AudioSegment.from_file("C:/Users/calvi/OneDrive - University of Iowa/Documents/aerobictextreview/3 Beginner Videos/Videos/Video_1_trimmed.wav")
+
+#play(vid_1)
+
+vid_1_distort = vid_1 * 10
+
+vid_1_distort.export("C:/Users/calvi/OneDrive - University of Iowa/Documents/aerobictextreview/3 Beginner Videos/Videos/Video_1_distort.wav", format="wav")
+
+
