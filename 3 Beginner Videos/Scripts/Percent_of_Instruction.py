0eaadbdb1033cc5f07d3a703ca965bc7b3ac0af1
Calvin Skalla <calvin-skalla@uiowa.edu>
2021-04-25 16:55:08 -0500

    Still working on manual audio labels
    Former-commit-id: 2a5a54164efa86a8433525db76f0d3dcf8a85e87

--- 3 Beginner Videos/Scripts/Percent_of_Instruction.py
+++ 3 Beginner Videos/Scripts/Percent_of_Instruction.py
@@ -5,45 +5,39 @@ import pandas as pd
 import sys
 
 
-def main():
+        
+filename = sys.argv[1]
     
-    script = sys.argv[0]
-    filename = sys.argv[1]
-    
-    # read audio samples using frame rate and # of frames
-    input_data = read(filename)
+# read audio samples using frame rate and # of frames
+input_data = read(filename)
        
-    # extract solely the frames clipped
-    audio = pd.DataFrame(input_data[1])
+# extract solely the frames clipped
+audio = pd.DataFrame(input_data[1])
     
-    #Delete the second repetitive column
-    audio2 = audio.drop(columns=[1])
+#Delete the second repetitive column
+audio2 = audio.drop(columns=[1])
     
-    #Delete zero values
-    audio3 = audio2[audio2 != 0]
+#Delete zero values
+audio3 = audio2[audio2 != 0]
     
-    #Change negative values to positive inverse to calculate log()
-    audio4 = np.where(audio3 < 0, (1/audio3) * -1, audio3)
+#Change negative values to positive inverse to calculate log()
+audio4 = np.where(audio3 < 0, (1/audio3) * -1, audio3)
     
-    #Convert values from amplitude to dB
-    audio_db = pd.DataFrame(20 * np.log10(audio4))
-    #audio_db = audio_db[audio_db != ]
+#Convert values from amplitude to dB
+audio_db = pd.DataFrame(20 * np.log10(audio4))
+#audio_db = audio_db[audio_db != ]
     
-    #Filter out dB not in 
-    audio_voice = audio_db.loc[(audio_db[0] <= 60) & (audio_db[0] >= 20)]
-    
-    
-    #Length of instruction
-    sec_instruct = len(audio_voice) / (input_data[0])
-    
-    
-    #length of video in seconds                                
-    video_1_length = len(input_data[1]) / (input_data[0])
+#Filter out dB not in 
+audio_voice = audio_db.loc[(audio_db[0] <= 60) & (audio_db[0] >= 20)]
+
+#Length of instruction
+sec_instruct = len(audio_voice) / (input_data[0])
     
     
-    #Percent of time instructing
-    perc_instruct = (sec_instruct / video_1_length) * 100
-    print(perc_instruct)
+#length of video in seconds                                
+video_1_length = len(input_data[1]) / (input_data[0])
     
-if __name__ == '__main__':
-    main()
+    
+#Percent of time instructing
+perc_instruct = (sec_instruct / video_1_length) * 100
+print(perc_instruct)
