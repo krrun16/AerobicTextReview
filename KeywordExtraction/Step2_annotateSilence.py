86bf3409f9a64102a82a9320899279cdd72f1559
Arely Sun <arely-sun@uiowa.edu>
2021-07-20 09:04:07 -0700

    Added code for improving accuracy of silence percentage

--- KeywordExtraction/Step2_annotateSilence.py
+++ KeywordExtraction/Step2_annotateSilence.py
@@ -1,28 +1,48 @@
 import csv
 
+
 def getPercentageSilence(cleanTimestampsFilename):
-    cleanTimestampsCSV=open(cleanTimestampsFilename)
+    cleanTimestampsCSV = open(cleanTimestampsFilename)
     reader = csv.reader(cleanTimestampsCSV)
-    allRows=[]
+    allRowsIncludingEmpty = []
+    allRows = []
 
     for row in reader:
+        allRowsIncludingEmpty.append([row[0], row[2], row[3]])
         if row[2] != "" and row[3] != "":
-            allRows.append([row[0],row[2],row[3]])
+            allRows.append([row[0], row[2], row[3]])
 
-    totalTime=allRows[len(allRows)-1][2]
+    totalTime = allRows[len(allRows) - 1][2]
 
-    totalSilence=0
-    for i in range(len(allRows)-1):
+    totalSilence = 0
+    for i in range(len(allRows) - 1):
         try:
-            nextFloatNumber=float(allRows[i+1][1])
-            firstFloatNumber=float(allRows[i][2])
-            totalSilence += nextFloatNumber-firstFloatNumber
+            nextFloatNumber = float(allRows[i + 1][1])
+            firstFloatNumber = float(allRows[i][2])
+            totalSilence += nextFloatNumber - firstFloatNumber
         except:
-            totalSilence+=0
+            totalSilence += 0
 
-    print("TOTAL SILENCE: "+str(totalSilence))
+    lastEndTime = float(allRowsIncludingEmpty[1][2])
+    previousWordHasNoTimestamp = False
+    totalSkipTime = 0
+    for i in range(1, len(allRowsIncludingEmpty)):
+        if allRowsIncludingEmpty[i][1] == "" or allRowsIncludingEmpty[i][2] == "":
+            previousWordHasNoTimestamp = True
+        else:
+            if previousWordHasNoTimestamp == True:
+                totalSkipTime = totalSkipTime + float(allRowsIncludingEmpty[i][1]) - lastEndTime
+                lastEndTime = float(allRowsIncludingEmpty[i][2])
+                previousWordHasNoTimestamp = False
+            else:
+                lastEndTime = float(allRowsIncludingEmpty[i][2])
+
+    totalTime = totalTime - totalSkipTime
+    totalSilence = totalSilence - totalSkipTime
+
+    print("TOTAL SILENCE: " + str(totalSilence))
     print("TOTAL TIME: " + str(totalTime))
-    return round((float(totalSilence)/float(totalTime))*100,2)
+    return round((float(totalSilence) / float(totalTime)) * 100, 2)
 
 import os
 
